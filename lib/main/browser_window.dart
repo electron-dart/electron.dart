c387872e285b4c4bcce79ce0a67546543f52667e
Kleak <segaud.kevin@gmail.com>
2016-09-15 09:45:14 +0200

    change license and header and strong-mode

--- lib/main/browser_window.dart
+++ lib/main/browser_window.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, GrimShield. All rights reserved. Use of this source code
+// Copyright (c) 2016, electron.dart. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 part of electron_main;
