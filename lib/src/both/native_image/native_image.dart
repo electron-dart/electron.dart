cc9b0d3834f74264d3dae5e6db3f39fd369fc4aa
Ravi Teja Gudapati <tejainece@gmail.com>
2016-09-15 13:29:07 +0200

    Updated copyright header

--- lib/src/both/native_image/native_image.dart
+++ lib/src/both/native_image/native_image.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2016, electron.dart. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
 @JS()
 library electron.both.native_image;
 
--- lib/src/main/app/app.dart
+++ lib/src/main/app/app.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2016, Ravi Teja Gudapati. All rights reserved. Use of this source code
+// Copyright (c) 2016, electron.dart. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 @JS()
@@ -24,8 +24,10 @@ void _requireElectron() {
 class NativeJsApp extends NativeJsEventEmitter {
   external void quit();
 }
+
 @JS("_electron.app")
 external NativeJsApp get _app;
+
 class App extends EventEmitter {
   NativeJsApp _app;
   StreamController<Null> _windowAllClosed;
@@ -43,6 +45,7 @@ class App extends EventEmitter {
     _ready = new StreamController<Null>(sync: true);
     on('ready', _onReady);
   }
+
   void _onWindowAllClosed([dynamic event]) => _windowAllClosed.add(null);
   void _onReady([dynamic event]) => _ready.add(null);
 }
